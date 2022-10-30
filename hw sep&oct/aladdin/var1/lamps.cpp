c63ea872ae183b16850d8f4946c43e2a1c1a3c37
lili09053 <91773645+lili09053@users.noreply.github.com>
2022-10-30 23:04:14 +0500

    Add files via upload

---
+++ hw sep&oct/aladdin/var1/lamps.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	system("chcp 1251>nul");
+	float v1, v2, v3, v4, v5;
+	cin >> v1 >> v2 >> v3 >> v4 >> v5;
+	
+	if (v1 > v2 && v1 > v3 && v1 > v4 && v1 > v5)
+	{
+		cout << "Лампа L1 самая волшебная" << endl;
+	}
+	else
+	{
+		if (v2 > v3 && v2 > v4 && v2 > v5)
+		{
+			cout << "Лампа L2 самая волшебная" << endl;
+		}
+		else
+		{
+			if (v3 > v4 && v3 > v5)
+			{
+				cout << "Лампа L3 самая волшебная" << endl;
+			}
+			else
+			{
+				if (v4 > v5)
+				{
+					cout << "Лампа L4 самая волшебная" << endl;
+				}
+				else
+				{
+					cout << "Лампа L5 самая волшебная" << endl;
+				}
+			}
+		}
+	}
+}   
+
