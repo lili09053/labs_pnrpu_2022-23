f437152cba02509d6dc5e5f26a6b47622bfbae90
lili09053 <91773645+lili09053@users.noreply.github.com>
2022-11-11 00:41:12 +0500

    Add files via upload

---
+++ hw nov/cycle/lamps/lamps_cycle.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    
+    float volume[5];              
+    for (int i = 0; i < 5; i++)         // создание массив
+        cin >> volume[i];
+    
+    int size = sizeof(volume) / sizeof(volume[0]); /* sizeof вероятнее всего работает с памятью массива, поэтому необходимо общую занимаемую массивом память поделить на обьем памяти, занимаемым  одним элементом массива */
+        
+    /* проверка массива
+    for (int i = 0; i < size; i++)
+        cout << volume[i] << endl;
+    */
+
+    float max_value = 0;
+    int num = 0;            //номер лампы
+    
+    
+    
+    for (int i = 0; i < size; i++) // цикл for
+    {
+        if (volume[i] > max_value)
+        {
+            max_value = volume[i];
+            num = i;
+        }
+    } 
+    cout << "Проверка циклом FOR: " << "Лампа " << ++num << " имеет набольший объём волшебства, равный " << max_value << endl; // ++ num - префикс, т.е операция +1 выполнится раньше, чем выведется знасение num
+
+    int i = 0;
+    max_value = 0;
+    num = 0;
+
+   
+    while (i < size)
+    {
+        if (volume[i] > max_value)
+        {
+            max_value = volume[i];
+            num = i;
+        }
+        i++;
+
+    }
+    cout << "Проверка циклом WHILE: " << "Лампа " << ++num << " имеет набольший объём волшебства, равный " << max_value << endl;
+
+    i = 0;
+    max_value = 0;
+    num = 0;        
+    do
+    {
+        if (volume[i] > max_value)
+        {
+            max_value = volume[i];
+            num = i;
+        }
+        i++;  
+    }
+    while (i < size);
+
+    cout << "Проверка циклом  DO WHILE: " << "Лампа " << ++num << " имеет набольший объём волшебства, равный " << max_value << endl;
+
+
+}
