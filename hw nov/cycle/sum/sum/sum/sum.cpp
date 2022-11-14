f7d41008524ffd1f58329a8791a601a3b441abe1
lili09053 <91773645+lili09053@users.noreply.github.com>
2022-11-14 10:50:04 +0500

    Add files via upload

---
+++ hw nov/cycle/sum/sum/sum/sum.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+
+    int l;
+    cin >> l;
+    int s = 0;
+
+    for (int i = 1; i <= l; i++)
+        s += i * (pow(-1, i));
+
+    cout << "Результат выполнения. Цикл FOR: " << s << endl;
+
+    s = 0;
+    int i = 1;
+
+    while (i <= l)
+    {
+        s += i * (pow(-1, i));
+        i++;
+    }
+    
+    cout << "Результат выполнения. Цикл WHILE: " << s << endl;
+
+    i = 1;
+    s = 0;
+    do
+    {
+        s += i * (pow(-1, i));
+        i++;
+    } 
+    while (i <= l);
+
+    cout << "Результат выполнения. Цикл DO WHILE: " << s << endl;
+
+
+}
+
---
+++ hw nov/cycle/sum/sum2/sum_2/sum_2/sum_2.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int l;
+	cin >> l;
+	int s = 0;
+
+	for (int i = 1; i <= l; i++)
+	{
+		if (i % 3 == 0)
+			s -= i;
+		else
+			s += i;
+	}
+	cout << "Результат выполнения. Цикл FOR: " << s << endl;
+
+	int i = 1;
+	s = 0;
+
+	while (i <= l)
+	{
+		if (i % 3 == 0)
+			s -= i;
+		else
+			s += i;
+		i++;
+	}
+
+	cout << "Результат выполнения. Цикл WHILE: " << s << endl;
+
+	i = 0;
+	s = 0;
+
+	do
+	{
+		{
+			if (i % 3 == 0)
+				s -= i;
+			else
+				s += i;
+			i++;
+		}
+	}
+	while (i <= l);
+	
+	cout << "Результат выполнения. Цикл DO WHILE: " << s << endl;
+
+}
+
+
