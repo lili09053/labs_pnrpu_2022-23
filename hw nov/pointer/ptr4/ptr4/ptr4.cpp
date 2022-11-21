0ffb3c688e97ecc59dafc1cc6f465c2329d09143
lili09053 <91773645+lili09053@users.noreply.github.com>
2022-11-21 10:24:12 +0500

    Add files via upload

---
+++ hw nov/pointer/ptr4/ptr4/ptr4.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    
+    int a;
+    int *ptra = &a;
+    cout << "Введите значение переменной a:\t";
+    cin >> *ptra;
+     
+    cout << "____________________ БЛОК ПРОВЕРКИ #1___________________________" << endl << endl;
+    cout << "&a\t" << &a << endl;
+    cout << "ptra\t" << ptra << endl;
+    cout << "*ptra\t" << *ptra << endl;
+    cout << "a\t" << a << endl;
+    cout << "==============================================================" << endl << endl;
+
+    int **ptr_ptra = &ptra;
+    cout << **ptr_ptra << endl;
+    cout << "Введите новое значение переменной a:\t";
+    cin >> **ptr_ptra;
+
+
+    cout << "____________________ БЛОК ПРОВЕРКИ #2___________________________" << endl << endl;
+    cout << "&a\t" << &a << endl;
+    cout << "ptra\t" << ptra << endl;
+    cout << "*ptra\t" << *ptra << endl;
+    cout << "a\t" << a << endl;
+    cout << "ptr_ptra\t" << ptr_ptra << endl;
+    cout << "**ptr_ptra\t" << **ptr_ptra << endl;
+    cout << "a\t" << a << endl;
+    cout << "==============================================================" << endl << endl;
+   
+
+}
+
