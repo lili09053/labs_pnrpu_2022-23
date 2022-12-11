6ce7bf1c6c2aaa8a30e4e2855851bee7baf19822
lili09053 <91773645+lili09053@users.noreply.github.com>
2022-12-11 19:24:16 +0500

    Add files via upload

---
+++ lesson dec/sum/sum/sum.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int s = 0;
+    for (int i = 1; i <= n; i++)
+
+    {
+        int p = 1;
+        for (int j = 1; j <= n; j++)
+        { 
+            p *= j;
+
+        }
+        s += p;
+    }
+
+    cout << s;
+}
+
