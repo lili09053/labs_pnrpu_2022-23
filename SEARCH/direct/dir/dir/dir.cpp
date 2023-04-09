8f0232f6dcd7e36130e76476765cb9c64ec5647c
lili09053 <91773645+lili09053@users.noreply.github.com>
2023-04-10 00:33:02 +0500

    Add files via upload

---
+++ SEARCH/direct/dir/dir/dir.cpp
@@ -0,0 +1,51 @@
+﻿// прямой поиск подстроки в строке
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	system("chcp 1251>NULL");
+
+	string str;
+	cout << "Введите строку: ";
+	getline(cin, str);
+
+	string SubStr;
+	cout << "Введите подстроку: ";
+	getline(cin, SubStr);
+	bool fl = false;
+	
+	int res = 0;
+
+	for (int i = 0; i < str.size() - SubStr.size() + 1; i++)
+	{
+		int k = 0;
+		while (k < SubStr.size())
+		{
+			if (str[i + k] != SubStr[k])
+			{
+				k = SubStr.size(); //принудительный выход
+			}
+			else
+			{
+				if (k == SubStr.size() - 1)
+				{
+					fl = true;
+					res = i;
+				}
+
+			}
+			k++;
+		}			
+	}
+	
+
+	if (fl)
+		cout << "Подстрока есть в строке!" << " начиная с " << res + 1 <<" -го символа" << endl;
+	else
+		cout << "Подстроки нет в строке!" << endl;
+
+}
+
---
+++ SEARCH/interpol/inter/inter/inter.cpp
@@ -0,0 +1,93 @@
+﻿// интерполяционный поиск
+
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+
+void SORT(int* m, int start, int end) // быстрая сортировка
+{
+    int i = start;
+    int j = end;
+
+    int x = m[(i + j) / 2];
+
+    while (i <= j)
+    {
+        while (m[i] < x)
+            i++;
+        while (m[j] > x)
+            j--;
+        if (i <= j)
+        {
+            swap(m[i], m[j]);
+            i++;
+            j--;
+        }
+    }
+    if (j > start)
+        SORT(m, start, j);
+    if (i < end)
+        SORT(m, i, end);
+
+}
+
+
+int main()
+{
+	bool fl = false;
+
+
+    int SIZE;
+
+	cout << "Enter N: ";
+	cin >> SIZE;
+	int* mas = new int[SIZE];
+
+	srand(time(NULL));
+
+	for (int i = 0; i < SIZE; i++)
+		mas[i] = rand() % 31 - 20;
+
+    SORT(mas, 0, SIZE - 1);
+
+    for (int i = 0; i < SIZE; i++)
+        cout << mas[i] << " ";
+    cout << endl;
+
+
+    int key;
+    cout << "Enter key: ";
+    cin >> key;
+
+    int left = 0;
+    int right = SIZE;
+
+    
+    while (left <= right and fl == false)
+    {
+        int mid = left + ((key - mas[left]) * (right - left)) / (mas[right] - mas[left]);
+        if (mas[mid] < key)
+        {
+            left = mid + 1;
+        }
+        else
+        {
+            if (mas[mid] > key)
+                right = mid + 1;
+            else
+                fl = true;
+        }
+    }
+
+    if (key == mas[left])
+        cout << "Index: " << left;
+    else
+    {
+        if (key == mas[right])
+            cout << "Index: " << right;
+    }
+
+
+	delete[] mas;
+}
