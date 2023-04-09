c8d11df37dfaf9ce0a9e8ef5639b5df853144dd9
lili09053 <91773645+lili09053@users.noreply.github.com>
2023-04-09 20:07:51 +0500

    Add files via upload

---
+++ LabNum7/Num7.1/num7.1/num7.1/num7.1.cpp
@@ -0,0 +1,88 @@
+﻿/*
+    Написать перегруженные функции и основную программу, которая их вызывает.
+        а) для массива целых чисел находит максимальный элемент;
+        б) для строки находит длину самого длинного слова.
+*/
+#include <iostream>
+#include <ctime>
+#include <string>
+
+using namespace std;
+
+
+int Max(int* m, int N) // нахождение макимального элемента
+{
+	int max = m[0];
+
+	for (int i = 1; i < N; i++)
+	{
+		if (m[i] > max)
+			max = m[i];
+	}
+	return max;
+}
+
+
+string Max(string str)
+{
+	string word;			// "временное" слово
+	string maxWord = "";	// максимальное по длине слово
+	
+	for (int i = 0; i < str.length(); i++) // проход по всей длине строки
+	{
+		
+		if (str[i] != ' ' and str[i] != '.' and str[i] != '!' and str[i] != '?' and str[i] != ':' and str[i] != ';')// "вычленение" слова из строки без учета знаков 
+			word += str[i];	// составление слова побуквенно
+		else
+		{
+			if (word.length() > maxWord.length())
+			{
+				maxWord = word;
+			}
+			word.clear();	// "обнуление" переменной
+			//word1 += '\0';
+		}
+	}
+	
+	if (word.length() > maxWord.length()) // проверка последнего введённого слова
+	{
+		maxWord = word;
+	}
+	word.clear();
+	return maxWord;
+}
+
+int main()
+{
+	cout << "--------------------------------- INTEGER -------------------------------------" << endl;
+	int N;
+	cout << "Enter N: ";
+	cin >> N;
+
+	srand(time(NULL));
+	int* m = new int[N];
+	for (int i = 0; i < N; i++) m[i] = rand() % 101 - 50;
+
+	for (int i = 0; i < N; i++) cout << m[i] << " ";
+	cout << endl;
+
+	cout << "Max elem: ";
+	cout << Max(m, N) << endl;
+	delete[] m;
+	
+	cout << "--------------------------------- STRING --------------------------------------" << endl;
+	
+	system("chcp 1251>NULL");
+
+	string STR;
+	cout << "Enter STRING: ";
+	getline(cin, STR);
+	getline(cin, STR);
+	
+	cout << "Max word: ";
+	cout << Max(STR) << endl;
+
+
+
+}
+
---
+++ LabNum7/Num7.2/num7.2/num7.2/num7.2.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <stdarg.h>
+using namespace std;
+
+
+int NOD_TWO(int a, int b) // расчёт НОД по алгоритму Евклида между 2мя числами
+{
+	if (a != 0 and b != 0)
+	{
+		if (a > b) return NOD_TWO(a % b, b);
+		if (a < b) return NOD_TWO(a, b % a);
+	}
+	return a + b;
+
+}
+
+
+int NOK_TWO(int a, int b)		// расчёт НОK по алгоритму Евклида между 2мя числами
+{
+	return a * b / NOD_TWO(a, b);
+}
+
+int NOK_SET(int n, ...) // Нахождение НОК множества чисел
+{
+	va_list fac;	// тип указателя, на список аргументов
+	va_start(fac, n); // задаёт указатель на первый необязательный элемент
+	
+	int res = NOK_TWO(va_arg(fac, int), va_arg(fac, int)); // НОК первых двух параметров
+	
+	for (int i = 0; i < n - 2; i++) // n - 2 т.к. первые два параметра рассмотрены
+	{
+		res = NOK_TWO(res, va_arg(fac, int)); // НОК между результатом и следующим параметром
+	}
+
+	va_end(fac); // сбрасывание указателя
+	
+	return res;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	cout << "НОК для трёх параметров: ";
+	cout << NOK_SET(3, 10, 14, 35) << endl; //70
+	
+	cout << "НОК для пяти параметров: ";
+	cout << NOK_SET(5, 6, 84, 48, 7, 143) << endl; // 48048
+
+	cout << "НОК для шести параметров: ";
+	cout << NOK_SET(6, 6, 45, 30, 60, 120, 135) << endl;//1080
+
+}
